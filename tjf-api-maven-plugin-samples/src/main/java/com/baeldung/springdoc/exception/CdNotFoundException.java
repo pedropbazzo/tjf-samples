e23cd61c14d0e6864f2a00ee7b18a284a498ed29
Marcos Paulo <marcos.paulo@totvs.com.br>
2020-05-26 16:05:31 -0300

    sample maven plugin openapi padrao totvs

---
+++ tjf-api-maven-plugin-samples/src/main/java/com/baeldung/springdoc/exception/CdNotFoundException.java
@@ -0,0 +1,10 @@
+package com.baeldung.springdoc.exception;
+
+@SuppressWarnings("serial")
+public class CdNotFoundException extends RuntimeException {
+
+    public CdNotFoundException() {
+
+    }
+
+}
