3a753b609586d34e76855d7d85d718c56f9a4fd7
Luca Fenris <lucaelert@gmail.com>
2020-05-28 09:19:51 -0300

    feat: Criar sample do openapi

---
+++ tjf-openapi/tjf-plugin-openapi/src/test/java/com/tjf/sample/github/TJFOpenapiApplication.java
@@ -0,0 +1,13 @@
+package com.tjf.sample.github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TJFOpenapiApplication {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
